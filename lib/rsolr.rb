8ddadff74006aa22ce44c50d27de0e64ef35a591
http://jneen.net/ <jneen@jneen.net>
2014-06-18 03:03:14 -0700

    move version info to its own file
    this fixes load errors when pulling it in via git dependency

--- lib/rsolr.rb
+++ lib/rsolr.rb
@@ -2,10 +2,6 @@ module RSolr
   
   Dir.glob(File.expand_path("../rsolr/*.rb", __FILE__)).each{|rb_file| require(rb_file)}
   
-  def self.version; "1.0.9" end
-  
-  VERSION = self.version
-  
   def self.connect *args
     driver = Class === args[0] ? args[0] : RSolr::Connection
     opts = Hash === args[-1] ? args[-1] : {}
---
+++ lib/rsolr/version.rb
@@ -0,0 +1,7 @@
+module RSolr
+  def self.version
+    "1.0.9"
+  end
+
+  VERSION = self.version
+end
